20982f73bfb67a33490ad25f8f8f5a3bbccc44ba
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2026-01-05 02:48:50 +0100

    Updates esbuild 0.25.0 -> 0.27.2, , babel core & preset-env 7.26.9 -> 7.28.5

--- test/fixtures/option-define/expected/index.js
+++ test/fixtures/option-define/expected/index.js
@@ -1,1 +1,1 @@
-(()=>{var e=["hello",1];var s={hello:"world"};window.env={strValue:"a string",boolValue:!1,numValue:66,arrValue:e,objValue:s};})();
+(()=>{var e=["hello",1];var o={hello:"world"};window.env={strValue:"a string",boolValue:!1,numValue:66,arrValue:e,objValue:o};})();
